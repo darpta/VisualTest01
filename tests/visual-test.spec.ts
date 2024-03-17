5079fc28121c479b66e0c878f5377a8a4dd01b55
Dariusz Ptak <dariusz.ptak@op.pl>
2024-03-17 18:33:21 +0100

    visual test update 2

--- tests/visual-test.spec.ts
+++ tests/visual-test.spec.ts
@@ -1,8 +1,13 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Visual Regression Testing Example", () => {
+//   test("Visual comparisons basic test", async ({ page }) => {
+//     await page.goto("https://www.24mx.pl");
+//     await expect(page).toHaveScreenshot("homepage.png");
+//   });
+
   test("Visual comparisons basic test", async ({ page }) => {
-    await page.goto("https://www.24mx.pl");
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
     await expect(page).toHaveScreenshot("homepage.png");
   });
 });
